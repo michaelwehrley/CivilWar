5279fa481eec470f3f22deed24f150b89060af7d
Michael Wehrley <michael.wehrley@gmail.com>
2017-12-02 17:45:36 -0600

    Add border states

--- assets/javascripts/date.js
+++ assets/javascripts/date.js
@@ -5,7 +5,7 @@
   // March 11, 1861 the Confederate Constitution of seven state signatories – South Carolina, Mississippi, Florida, Alabama, Georgia, Louisiana, and Texas – replaced the February 7 Provisional Confederate States Constitution with one stating in its preamble a desire for a "permanent federal government".
   // War: April 12, 1861 – May 9, 1865
 
-  var START_DATE = "12-20-1860";
+  var START_DATE = "11-06-1860";
 
   CivilWar.dates = {
     selected: moment(START_DATE, "MM-DD-YYYY"),
